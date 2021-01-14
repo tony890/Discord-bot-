94970c54c3c92e7d4ba2211226b7073b960911e9
tony890 <51936070+tony890@users.noreply.github.com>
2021-01-13 20:32:17 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ require('dotenv').config()
 const Discord = require('discord.js');
 const config = require('./config.json');
 const client = new Discord.Client();
-
 client.once('ready', () => {
 	console.log('Ready!');
 });
@@ -26,9 +25,6 @@ client.on('message', message => {
         message.reply(message.author.displayAvatarURL());
   }
 });   
-client.on('messageDelete', message => {
-    message.reply('Stop deleting messages!!😡😡');
-});
 client.on('guildMemberRemove', member => {
 	console.log(`${member.user.tag} left the guild... but was it of their own free will?`);
 });
@@ -36,4 +32,13 @@ client.on('guildBanAdd', async (guild, user) => {
 	console.log(`${user.tag} got hit with the ban hammer ${guild.name}.`);
 });
 
+client.on('message', message => {
+	if(message.author.bot) return;
+	if(config.FILTER_LIST.some(word => message.content.toLowerCase().includes(word))){
+	  message.delete()
+	}})
+	client.on('messageDelete', message => {
+		const channel01 = client.channels.cache.find(channel => channel.id === "799076668353413120");
+	channel01.send(message.content)
+	});
 client.login(process.env.BOT_TOKEN)
